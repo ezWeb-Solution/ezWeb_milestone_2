1308e4a0649402a82394710b4c8ae407b3024685
seanmanik <seanmanik@gmail.com>
2022-07-21 23:02:29 +0800

    Edited Excel Sheet fields
    Fields are edited to ensure parity with current listing creation fields.

--- ezWeb/ezWeb Frontend/ExcelParser.py
+++ ezWeb/ezWeb Frontend/ExcelParser.py
@@ -1,4 +1,5 @@
 
+import time
 import pandas as pd
 # Open excel file and read
 workbook = pd.read_excel('sample-xlsx-file-for-testing.xlsx', sheet_name="Sheet1")
@@ -9,13 +10,14 @@ curr_row = 2
 while workbook['Property Name'].iloc[curr_row] is not None:
     listings.append({})
     new_listing = listings[curr_row - 2]
-    new_listing['property_name'] = workbook['Property Name'].iloc[curr_row]
     new_listing['property_type'] = workbook['Property Type'].iloc[curr_row]
+    new_listing['year_built'] = workbook['Year Built'].iloc[curr_row]
     new_listing['price'] = workbook['Price'].iloc[curr_row]
-    new_listing['num_bedrooms'] = workbook['Num. of Bedrooms'].iloc[curr_row]
-    new_listing['num_bathrooms'] = workbook['Num. of Bathrooms'].iloc[curr_row]
-    new_listing['num_floors'] = workbook['Num. of Floors'].iloc[curr_row]
-    new_listing['size'] = workbook['Size (sqft)'].iloc[curr_row]
+    new_listing['address'] = workbook['Address'].iloc[curr_row]
+    new_listing['tenure'] = workbook['Tenure'].iloc[curr_row]
+    new_listing['num_rooms'] = workbook['Num. of Rooms'].iloc[curr_row]
+    new_listing['number_of_storeys'] = workbook['Num. of Floors'].iloc[curr_row]
+    new_listing['total_area'] = workbook['Total Area (sqft)'].iloc[curr_row]
     new_listing['floor'] = workbook['Floor'].iloc[curr_row]
     new_listing['featured_photo'] = workbook['Featured Photo'].iloc[curr_row]
     other_photos = workbook['Other Photos'].iloc[curr_row]
@@ -29,19 +31,21 @@ print(workbook['Property Name'].iloc[0])
 # Output
 for listing in listings:
     ## Calls create listing and sends listing to the backend.
-    create_new_listing(listing)
+    #create_new_listing(listing)
+    time.sleep(10)
 workbook.head()
 
+
 # Create a new file
-name = ['John', 'Mary', 'Sherlock']
-age = [11, 12, 13]
-df = pd.DataFrame({ 'Property Name': [],
-                    'Property Type': [],
+
+df = pd.DataFrame({ 'Property Type': [],
+                    'Year Built': [],
                     'Price': [],
-                    'Num. of Bedrooms': [],
-                    'Num. of Bathrooms': [],
+                    'Address': [],
+                    'Tenure': [],
+                    'Num. of Rooms': [],
                     'Num. of Floors': [],
-                    'Size (sqft)': [],
+                    'Total Area (sqft)': [],
                     'Floor': [],
                     'Featured_Photo': [],
                     'Other Photos': []})
@@ -49,3 +53,4 @@ df = pd.DataFrame({ 'Property Name': [],
 # Use a hash to generate a file name.
 df.index.name = 'ID'
 df.to_excel('my_file.xlsx')
+
